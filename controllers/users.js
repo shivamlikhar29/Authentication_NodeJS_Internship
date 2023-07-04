74bb337e8d6741172f7f4cab33e29a2ed0b619f4
Shivam Likhar <112860512+shivamlikhar29@users.noreply.github.com>
2023-07-05 00:29:42 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -120,7 +120,7 @@ const newPassword= async(req,res)=>{
    
     try{
       const updatedPost = await User.findByIdAndUpdate(id,{password:hashedPassword});
-      res.status(2001).json({message:"password updated successfully",updatedPost})
+      res.status(201).json({message:"password updated successfully",updatedPost})
       
 
     }catch(err){
