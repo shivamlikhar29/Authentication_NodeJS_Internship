81d3481d173e8c1cd0ac07dac4c2a0d6dbb18f66
Shivam Likhar <112860512+shivamlikhar29@users.noreply.github.com>
2023-07-04 22:33:29 +0530

    Removed comments from server.js

--- server.js
+++ server.js
@@ -11,66 +11,6 @@ const routes = require('./routes/routes')
 app.set('view engine', 'ejs');
 app.use(express.json())
 
-// const users = [] //for storing users information
-
-app.use(bodyParser.json({limit:'30mb',extended:true}))
-app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
-
-// app.get('/users', (req, res) => {
-//     res.json(users)
-//     console.log(process.env.EMAIL)
-// })
-// app.post('/users', async (req, res) => {
-//     try {
-//       const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//       const user = { name: req.body.name, email:req.body.email, password: hashedPassword }
-//       users.push(user)
-//       res.status(201).send("Registered Successfully")
-//       console.log(users)
-//     } catch {
-//       res.status(500).send()
-//     }
-// })
-// app.post('/users/login', async (req, res) => {
-//     const user = users.find(user => user.email === req.body.email)
-//     if (user == null) {
-//       return res.status(400).send('Cannot find user')
-//     }
-//     try {
-//       if(await bcrypt.compare(req.body.password, user.password)) {
-//         res.send('Login Success')
-//       } else {
-//         res.send('Not Allowed')
-//       }
-//     } catch {
-//       res.status(500).send()
-//     }
-// })
-// app.patch('/users/forget-password', async (req, res) => {
-//     const user = users.find(user => user.email === req.body.email)
-//     if (user == null) {
-//       return res.status(400).send('Cannot find user')
-//     }
-//     let newPassword = Math.floor(Math.random() * 9000) + 1000
-//     try {
-
-//         const hashedPassword = await bcrypt.hash(newPassword.toString(), 10)
-//        const updatedUser =  users.map((user) => {
-//             if(user.email === req.body.email) {
-//                 user.password = hashedPassword
-//             }
-//             return user
-//         })
-//         users.push(updatedUser)
-//         sendEmail(req.body.email,newPassword)
-//         res.status(201).json({NewPassword : newPassword})
-//     } 
-//     catch {
-//       res.status(500).send()
-//     }
-// })
-
-
 app.use('/users',routes)
   
 
